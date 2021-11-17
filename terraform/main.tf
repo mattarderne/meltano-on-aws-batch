5ca58e19afe789764514c1976d19f394078dc3ea
mattarderne <23472325+mattarderne@users.noreply.github.com>
2021-11-17 21:28:44 +0200

    base terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,119 +14,261 @@ provider "aws" {
   region  = var.region
 }
 
-resource "aws_eip" "nat" {
-  count = 1
-
-  vpc = true
+# retrieves the default vpc for this region
+data "aws_vpc" "default" {
+  default = true
 }
 
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "${var.prefix}-${var.vpc_name}"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["${var.region}a", "${var.region}b", "${var.region}c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-  database_subnets    = ["10.0.21.0/24", "10.0.22.0/24"]
-
-
-  enable_nat_gateway  = true
-  single_nat_gateway  = true
-  one_nat_gateway_per_az = false
-  reuse_nat_ips       = true                    # <= Skip creation of EIPs for the NAT Gateways
-  external_nat_ip_ids = aws_eip.nat.*.id        # <= IPs specified here as input to the module
-
-# TODO: enables internet access to the DB, only necessary for development, 
-  create_database_subnet_group           = true
-  create_database_subnet_route_table     = true
-  create_database_internet_gateway_route = true
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-
-
-  tags = {
-    Terraform = "true"
-    Project = "${var.prefix}-meltano"
-  }
+# retrieves the subnet ids in the default vpc
+data "aws_subnet_ids" "all" {
+  vpc_id = data.aws_vpc.default.id
 }
 
+# helper to package the lambda function for deployment
+# data "archive_file" "lambda_zip" {
+#   type = "zip"
+#   source_file = "lambda/index.js"
+#   output_path = "lambda_function.zip"
+# }
 
-resource "aws_db_subnet_group" "subnet" {
-  name       = "${var.prefix}-${var.subnet_name}"
-  subnet_ids = module.vpc.public_subnets
+
+resource "aws_iam_role" "instance-role" {
+  name = "aws-batch-meltano-role"
+  path = "/BatchSample/"
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement":
+    [
+      {
+          "Action": "sts:AssumeRole",
+          "Effect": "Allow",
+          "Principal": {
+            "Service": "ec2.amazonaws.com"
+          }
+      }
+    ]
+}
+EOF
 }
 
-resource "aws_security_group" "security_group" {
-  name   = "${var.prefix}-${var.security_group_name}"
-  vpc_id = module.vpc.vpc_id
+resource "aws_iam_role_policy_attachment" "instance-role" {
+  role = aws_iam_role.instance-role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
 
-  ingress {
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-    cidr_blocks = var.access_ip_list
-    # cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_iam_instance_profile" "instance-role" {
+  name = "aws-batch-meltano-role"
+  role = aws_iam_role.instance-role.name
+}
+
+resource "aws_iam_role" "aws-batch-service-role" {
+  name = "aws-batch-service-role"
+  path = "/BatchSample/"
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement":
+    [
+      {
+          "Action": "sts:AssumeRole",
+          "Effect": "Allow",
+          "Principal": {
+            "Service": "batch.amazonaws.com"
+          }
+      }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "aws-batch-service-role" {
+  role = aws_iam_role.aws-batch-service-role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSBatchServiceRole"
+}
+
+resource "aws_security_group" "meltano-batch" {
+  name = "aws-batch-meltano-security-group"
+  description = "AWS Batch Sample Security Group"
+  vpc_id = data.aws_vpc.default.id
 
   egress {
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Terraform = "true"
-    Project = "${var.prefix}-meltano"
+    from_port       = 0
+    to_port         = 65535
+    protocol        = "tcp"
+    cidr_blocks     = [ "0.0.0.0/0" ]
   }
 }
 
-resource "aws_db_parameter_group" "parameters" {
-  name   = "${var.prefix}-${var.rds_parameters}"
-  family = "postgres13"
-
-  parameter {
-    name  = "log_connections"
-    value = "1"
+resource "aws_batch_compute_environment" "meltano" {
+  compute_environment_name = "meltano-sample"
+  compute_resources {
+    instance_role = aws_iam_instance_profile.instance-role.arn
+    instance_type = [
+      "optimal"
+    ]
+    max_vcpus = 6
+    min_vcpus = 0
+    security_group_ids = [aws_security_group.meltano-batch.id]
+    subnets = data.aws_subnet_ids.all.ids
+    type = "EC2"
   }
+  service_role = aws_iam_role.aws-batch-service-role.arn
+  type = "MANAGED"
+  depends_on = [ aws_iam_role_policy_attachment.aws-batch-service-role ]
 }
 
-resource "aws_db_instance" "db_instance" {
-  identifier             = var.rds_name
-  instance_class         = "db.t3.micro"
-  allocated_storage      = 5
-  engine                 = "postgres"
-  engine_version         = "13.3"
-  name                   = var.db_name
-  username               = var.db_username
-  password               = var.db_password
-  db_subnet_group_name   = aws_db_subnet_group.subnet.name
-  vpc_security_group_ids = [aws_security_group.security_group.id]
-  parameter_group_name   = aws_db_parameter_group.parameters.name
-  publicly_accessible    = true
-  skip_final_snapshot    = true
-  apply_immediately      = true
-  
-  tags = {
-    Terraform = "true"
-    Project = "${var.prefix}-meltano"
-  }
+resource "aws_batch_job_queue" "meltano" {
+  name = "meltano-queue"
+  state = "ENABLED"
+  priority = 1
+  compute_environments = [ aws_batch_compute_environment.meltano.arn ]
 }
 
-# resource "aws_elastic_beanstalk_application" "application" {
-#   name        = var.eb_application_name
-# }
+resource "aws_ecr_repository" "meltano-job-repo" {
+  name = "aws-batch-meltano-sample"
+}
 
-# resource "aws_elastic_beanstalk_environment" "environment" {
-#   name                = var.eb_environment_name
-#   application         = aws_elastic_beanstalk_application.application.name
-#   solution_stack_name = "64bit Amazon Linux 2 v3.4.8 running Docker"
-#     setting {
-#         namespace = "aws:autoscaling:launchconfiguration"
-#         name      = "IamInstanceProfile"
-#         value     = "aws-elasticbeanstalk-ec2-role"
+resource "aws_s3_bucket" "image-bucket" {
+  bucket_prefix = "aws-batch-sample-"
+}
+
+resource "aws_iam_role" "job-role" {
+  name = "aws-batch-meltano-job-role"
+  path = "/BatchSample/"
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement":
+    [
+      {
+          "Action": "sts:AssumeRole",
+          "Effect": "Allow",
+          "Principal": {
+            "Service": "ecs-tasks.amazonaws.com"
+          }
+      }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "job-policy" {
+  name = "aws-batch-meltano-job-policy"
+  path = "/BatchSample/"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:Get*"
+      ],
+      "Effect": "Allow",
+      "Resource": [
+        "${aws_s3_bucket.image-bucket.arn}",
+        "${aws_s3_bucket.image-bucket.arn}/*"
+      ]
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "job-role" {
+  role = aws_iam_role.job-role.name
+  policy_arn = aws_iam_policy.job-policy.arn
+}
+
+resource "aws_batch_job_definition" "meltano-job" {
+  name = "meltano-job"
+  type = "container"
+  depends_on = [
+    aws_ecr_repository.meltano-job-repo,
+    aws_s3_bucket.image-bucket,
+  ]
+  parameters = {
+    bucketName = aws_s3_bucket.image-bucket.id
+  }
+  container_properties = <<CONTAINER_PROPERTIES
+{
+  "image": "${aws_ecr_repository.meltano-job-repo.repository_url}",
+  "jobRoleArn": "${aws_iam_role.job-role.arn}",
+  "vcpus": 2,
+  "memory": 2000,
+  "environment": [
+    { "name": "AWS_REGION", "value": "${var.region}" }
+  ],
+  "command": [
+    "elt"
+  ]
+}
+CONTAINER_PROPERTIES
+}
+
+# ## lambda resource + iam
+# resource "aws_iam_role" "lambda-role" {
+#   name = "aws-batch-meltano-function-role"
+#   path = "/BatchSample/"
+#   assume_role_policy = <<EOF
+# {
+#     "Version": "2012-10-17",
+#     "Statement":
+#     [
+#       {
+#           "Action": "sts:AssumeRole",
+#           "Effect": "Allow",
+#           "Principal": {
+#             "Service": "lambda.amazonaws.com"
+#           }
 #       }
+#     ]
+# }
+# EOF
 # }
 
+# resource "aws_iam_policy" "lambda-policy" {
+#   name = "aws-batch-meltano-function-policy"
+#   path = "/BatchSample/"
+#   policy = <<EOF
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Action": [
+#         "batch:SubmitJob"
+#       ],
+#       "Effect": "Allow",
+#       "Resource": "*"
+#     }
+#   ]
+# }
+# EOF
+# }
+
+# resource "aws_iam_role_policy_attachment" "lambda-service" {
+#   role = aws_iam_role.lambda-role.name
+#   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+# }
+
+# resource "aws_iam_role_policy_attachment" "lambda-policy" {
+#   role = aws_iam_role.lambda-role.name
+#   policy_arn = aws_iam_policy.lambda-policy.arn
+# }
+
+# resource "aws_lambda_function" "submit-job-function" {
+#   function_name = "aws-batch-meltano-function"
+#   filename = "lambda_function.zip"
+#   role = aws_iam_role.lambda-role.arn
+#   handler = "index.handler"
+#   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+#   runtime = "nodejs14.x"
+#   depends_on = [ "aws_iam_role_policy_attachment.lambda-policy" ]
+#   environment {
+#     variables = {
+#       JOB_DEFINITION = aws_batch_job_definition.meltano-job.arn
+#       JOB_QUEUE = aws_batch_job_queue.meltano.arn
+#       IMAGES_BUCKET = aws_s3_bucket.image-bucket.id
+#     }
+#   }
+# }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,41 +1,13 @@
 
-output "rds_hostname" {
-  description = "RDS instance hostname"
-  value       = aws_db_instance.db_instance.address
-  sensitive   = true
-}
-
-output "rds_port" {
-  description = "RDS instance port"
-  value       = aws_db_instance.db_instance.port
-}
-
-output "rds_username" {
-  description = "RDS instance root username"
-  value       = aws_db_instance.db_instance.username
-  sensitive   = true
-}
-
-output "db_connect_string" {
-  description = "Posgresql database connection string"
-  value       = "postgresql://${var.db_username}:${var.db_password}@${aws_db_instance.db_instance.address}:${aws_db_instance.db_instance.port}/${var.db_name}"
-  sensitive   = true
-}
-
-output "security_group_id" {
-  description = "Security Group Name"
-  value       = aws_security_group.security_group.id
-}
-
 # output "apprunner_url" {
 #   description = "Apprunner application URL"
 #   value       = aws_apprunner_service.meltano-apprunner.service_url
 # }
 
 output "ecr_repository" {
-  value = "${aws_ecr_repository.meltano-job-repo.repository_url}"
+  value = aws_ecr_repository.meltano-job-repo.repository_url
 }
 
 output "image_bucket" {
-  value = "${aws_s3_bucket.image-bucket.id}"
+  value = aws_s3_bucket.image-bucket.id
 }
